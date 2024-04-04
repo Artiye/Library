85b26a8bfd3edc2a5ae3df4964aa9bd97ea49d40
Artiye <artinjobro@gmail.com>
2024-04-04 16:36:26 +0200

    Added encryption for the Member Service

--- BackEnd/Library.Application/Services/MemberService.cs
+++ BackEnd/Library.Application/Services/MemberService.cs
@@ -2,6 +2,7 @@
 using Library.Application.DTOs.AuthorDTOs;
 using Library.Application.DTOs.BookDTOs;
 using Library.Application.DTOs.ProfileDTOs;
+using Library.Application.Encryption;
 using Library.Application.Services.Interfaces;
 using Library.Domain.Entity;
 using Microsoft.AspNetCore.Identity;
@@ -18,11 +19,13 @@ namespace Library.Application.Services
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMapper _mapper;
+        private readonly IEncryptionService _encryptionService;
 
-        public MemberService(UserManager<ApplicationUser> userManager, IMapper mapper )
+        public MemberService(UserManager<ApplicationUser> userManager, IMapper mapper, IEncryptionService encryptionService )
         {
             _userManager = userManager;
             _mapper = mapper;
+            _encryptionService = encryptionService;
         }
         public async Task<List<GetProfileDTO>> GetAllMembers()
         {
@@ -36,6 +39,13 @@ namespace Library.Application.Services
                 return new List<GetProfileDTO>();
             }
             var profilesDTO = _mapper.Map<List<GetProfileDTO>>(userList);
+            foreach(GetProfileDTO profiles in profilesDTO)
+            {
+                profiles.FirstName = _encryptionService.DecryptData(profiles.FirstName);
+                profiles.LastName = _encryptionService.DecryptData(profiles.LastName);
+                profiles.Gender = _encryptionService.DecryptData(profiles.Gender);
+                profiles.Nationality = _encryptionService.DecryptData(profiles.Nationality);
+            }
             return profilesDTO;
         }
 
@@ -47,6 +57,10 @@ namespace Library.Application.Services
                 throw new Exception("User does not exist");
 
             var userDTO = _mapper.Map<GetProfileDTO>(user);
+            userDTO.FirstName = _encryptionService.DecryptData(userDTO.FirstName);
+            userDTO.LastName = _encryptionService.DecryptData(userDTO.LastName);
+            userDTO.Gender = _encryptionService.DecryptData(userDTO.Gender);
+            userDTO.Nationality = _encryptionService.DecryptData(userDTO.Nationality);
             return userDTO;
         }
 
@@ -65,6 +79,12 @@ namespace Library.Application.Services
             
 
             var readListDTO = _mapper.Map<List<GetBookDTO>>(user.Books);
+            foreach(GetBookDTO books in readListDTO)
+            {
+                books.Title = _encryptionService.DecryptData(books.Title);
+                books.Description = _encryptionService.DecryptData(books.Description);
+                books.CoverImage = _encryptionService.DecryptData(books.CoverImage);
+            }
             return readListDTO;
         }
         
@@ -80,6 +100,13 @@ namespace Library.Application.Services
                 return new List<GetAuthorDTO>();
 
             var authorListDTO = _mapper.Map<List<GetAuthorDTO>>(user.Authors);
+            foreach(GetAuthorDTO authors in  authorListDTO)
+            {
+                authors.FullName = _encryptionService.DecryptData(authors.FullName);
+                authors.Nationality = _encryptionService.DecryptData(authors.Nationality);
+                authors.BioGraphy = _encryptionService.DecryptData(authors.BioGraphy);
+                authors.ProfileImage = _encryptionService.DecryptData(authors.ProfileImage);
+            }
             return authorListDTO;
         }
       
